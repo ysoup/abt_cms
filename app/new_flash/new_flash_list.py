4b7be334839fbdc526e52df0fcba272ab5f5b816
杨卫东 <yangwd@aibicoin.com>
2018-11-01 19:10:49 +0800

    增加导入功能

--- app/new_flash/new_flash_list.py
+++ app/new_flash/new_flash_list.py
@@ -1,20 +1,19 @@
 # encoding=utf-8
 from flask_login import login_required
-from flask import request, render_template, jsonify, flash, abort, url_for, redirect, session, Flask, g, current_app
+from flask import request, render_template, jsonify, make_response,flash, abort, url_for, redirect, session, Flask, g, current_app
 from . import new_flash
 from app.models import NewFlashInformation, NewFlashCategory, NewFlashRule, AccountManage
 from cms_server import db, redis_store
 import time, copy
 from common import push_service
 import json, datetime
-
+import os
+import xlrd
 # utils相关
 from ..utils.filter_char import filter_char
 from ..utils.sphinxapi3_query_keyword import query_keyword
 from ..utils.Pagination import Pagination
-ORIGINAL ='ren_gong'
-BGMARKET ='aibitou_market'
-SELECT_ID ='select_id'
+from werkzeug.utils import secure_filename
 
 
 # 快讯列表页
@@ -30,6 +29,37 @@ def account_manage_list():
     except Exception as e:
         current_app.logger.error(e)
         return render_template("404.html")
+
+
+# 文件上传
+@new_flash.route('/file_upload', methods=['POST'])
+@login_required
+def file_upload():
+    try:
+        file_dict = request.files["file_data"]
+        filename = secure_filename(file_dict.filename)
+        file_dict.save(os.path.join("./", filename))
+        xls_file = xlrd.open_workbook(filename)
+        xls_sheet = xls_file.sheets()[0]
+        nrows = xls_sheet.nrows
+        ncols = xls_sheet.ncols
+        for i in range(1, nrows):
+            info = []
+            for j in range(0, ncols):
+                var = xls_sheet.cell(i, j).value
+                info.append(var)
+            # 数据库持久化
+            info = AccountManage(
+                account_name=info[0],
+                account_password=info[1],
+                account_rank=info[2],
+                account_article_num=info[3])
+            db.session.add(info)
+            db.session.commit()
+        return jsonify({"success": "ok"})
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify({"success": "failed"})
 
 
 
